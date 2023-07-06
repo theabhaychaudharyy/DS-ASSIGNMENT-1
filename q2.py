43a169d9a8d89a41a6d3028c3710c5009b2d434c
Abhay Chaudhary <112479526+theabhaychaudharyy@users.noreply.github.com>
2023-07-06 22:09:41 +0530

    Create q2.py

---
+++ q2.py
@@ -0,0 +1,15 @@
+def is_palindrome(string):
+    string = string.replace(" ", "").lower()
+    start = 0
+    end = len(string) - 1
+    while start < end:
+        if string[start] != string[end]:
+            return False
+        start += 1
+        end -= 1
+    return True
+input_string = input("Enter a string: ")
+if is_palindrome(input_string):
+    print("The string is a palindrome.")
+else:
+    print("The string is not a palindrome.")
